31cd6a218e6d2b10b1ff31356b209133cf9e2d1e
okami-ap <y.okami@alphapolis.co.jp>
2024-04-19 11:10:30 +0900

    クーポン画面でお気に入り情報あるか取得

--- app/src/main/java/jp/techacademy/youichi/okami/apiapp/WebViewActivity.kt
+++ app/src/main/java/jp/techacademy/youichi/okami/apiapp/WebViewActivity.kt
@@ -8,24 +8,41 @@ import jp.techacademy.youichi.okami.apiapp.databinding.ActivityWebViewBinding
 
 class WebViewActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWebViewBinding
-
+    var isFavorite: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWebViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // ActionBarの追加
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "クーポン詳細"
 
         binding.webView.loadUrl(intent.getStringExtra(KEY_URL).toString())
+
+        // Shop情報を受取
+        val id = intent.getStringExtra(KEY_ID).toString()
+        val name = intent.getStringExtra(KEY_NAME).toString()
+        val address = intent.getStringExtra(KEY_ADDRESS).toString()
+        val imageUrl = intent.getStringExtra(KEY_IMAGEURL).toString()
+
+        // お気に入り状態を取得
+        isFavorite = FavoriteShop.findBy(id) != null
     }
 
     companion object {
         private const val KEY_URL = "key_url"
+        private const val KEY_ID = "key_id"
+        private const val KEY_NAME = "key_name"
+        private const val KEY_ADDRESS = "key_address"
+        private const val KEY_IMAGEURL = "key_imageUrl"
         fun start(activity: Activity, url: String, id: String, name: String, address: String, imageUrl: String) {
-            activity.startActivity(
-                Intent(activity, WebViewActivity::class.java).putExtra(
-                    KEY_URL,
-                    url
-                )
-            )
+            val intent = Intent(activity, WebViewActivity::class.java)
+            intent.putExtra(KEY_URL, url)
+            intent.putExtra(KEY_ID, id)
+            intent.putExtra(KEY_NAME, name)
+            intent.putExtra(KEY_ADDRESS, address)
+            intent.putExtra(KEY_IMAGEURL, imageUrl)
+            activity.startActivity(intent)
         }
     }
 }
